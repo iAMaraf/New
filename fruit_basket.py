feab5730609aa6959103e91fc780d86b6311e2c5
root <root@localhost.localdomain>
2018-06-18 22:14:25 -0400

    added .py

---
+++ fruit_basket.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+fruit_basket = [apple, orange, banana, pear, plum]
+guessed_fruit = input ("Guess a fruit: ")
+
+if guessed_fruit == [apple, orange, banana, pear, plum]:
+    print ("Correct!")
+    Else:
+        print ("Guess again")
